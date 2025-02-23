0b0ca4e24f940df25850aa7cc3ed7a9695710467
Justin Cheung <jrcheung168@gmail.com>
2025-02-23 09:03:04 -0600

    fix build container. in-progress work.

--- src/standalones/advent_of_code/Day12b.cpp
+++ src/standalones/advent_of_code/Day12b.cpp
@@ -11,15 +11,8 @@ void record_next_state(TransitionMap& transitions, std::vector<u_int64_t>& count
     if (blink == numBlinks)
     {
         counts[stoneNumber] += 1;
-        // std::cout << counts[stoneNumber] << std::endl;
         return;
     }
-
-    else if (blink == 40)
-    {
-        std::cout << counts[stoneNumber] << std::endl;
-    }
-
 
     if (state == 0)
     {
@@ -38,10 +31,17 @@ void record_next_state(TransitionMap& transitions, std::vector<u_int64_t>& count
             if (transitions.find(state) == transitions.end())
             {
                 const u_int64_t breakPoint = static_cast<u_int64_t>(pow(10.0f, (stateLength / 2)));
-                // std::cout << "Breakpoint: " << std::to_string(state) << std::endl;
                 const u_int64_t nextStateRight = state % breakPoint;
                 const u_int64_t nextStateLeft = state / breakPoint;
                 transitions[state] = std::make_pair(nextStateLeft, nextStateRight);
+
+                if (transitions.size() == 41)
+                {
+                    for (const auto& pair : transitions)
+                    {
+                        std::cout << std::to_string(pair.first) << "," << std::to_string(pair.second.first) << "," << std::to_string(pair.second.second) << std::endl;
+                    }
+                }
             }
             record_next_state(transitions, counts, stoneNumber, numBlinks, transitions[state].first, blink+1);
             record_next_state(transitions, counts, stoneNumber, numBlinks, transitions[state].second, blink+1);
@@ -71,8 +71,6 @@ int main()
         record_next_state(transitions, counts, stoneNumber, numBlinks, stone, 0);
         std::cout << "Stone " << std::to_string(stone) << " has " << std::to_string(counts[stoneNumber]) << " children." << std::endl;
         summation += counts[stoneNumber];
-
-        
     }
 
     std::cout << "Summation: " + std::to_string(summation) << std::endl;
